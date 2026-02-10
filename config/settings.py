74f0e97471d594ea93d08861ad31cce68c03dcec
Armin <armin.lambda@gmail.com>
2026-02-10 10:24:30 +0330

    change / add something...

--- config/settings.py
+++ config/settings.py
@@ -134,6 +134,10 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
 # ----- My Configs -----
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
